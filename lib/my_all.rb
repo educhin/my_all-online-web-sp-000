a0b5e5224822a4e347cdd69db920fb000c2cc2f9
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-28 05:42:21 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,6 +7,9 @@ def my_all?(collection)
     block_return_values.push(yield(collection[i]))
     i += 1
   end
-  block_return_values.include?(false)
-    
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
